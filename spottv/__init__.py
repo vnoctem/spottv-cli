babf60c1a73745435957f8f9241073362e8cf6f5
Vincent <vincent.ngo.gauthier@gmail.com>
2022-01-16 18:38:31 -0500

    Implement textinput.py sample

---
+++ spottv/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import sys
+
+if sys.version_info.major < 3:
+    print("This program requires Python 3 and above to run.")
+    sys.exit(1)
+
+__author__ = "Vincent Gauthier"
+__email__ = "vincent.ngo.gauthier@gmail.com"
+__version__ = "1.0"
